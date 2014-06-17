aea2f22fa1b5fb6f63f7d3bcb5adefec30edaa86
gradinafrica <gradinafrica@yahoo.com>
2014-06-17 00:04:14 -0700

    Added a program usage text

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
 void main(int argc, char** argv)
 {
+	if (argc > 1 && !strcmp(argv[1], "/?"))
+	{
+		printf("%s", "Usage: bfc [source]\n\n");
+		printf("%s", "  [source]: A file containing a program written in Befunge-98\n");
+		return 0;
+	}
 
+	return 0;
 }
