5f0e4709ea473f73d88f22184188a9647c0108ba
gradinafrica <gradinafrica@yahoo.com>
2014-06-20 02:57:32 -0700

    Refactored the stack header to use an actual head node instead of a dummy node

--- Stack.h
+++ Stack.h
@@ -5,42 +5,44 @@
 
 struct Stack
 {
-	struct Node head;
+	struct Node* head;
 };
 
 void StackInit(struct Stack* stack)
 {
-	NodeInit(&stack->head, NULL);
+	stack->head = NULL;
 }
 
 void StackPush(struct Stack* stack, void* data)
 {
+	struct Node** headNode = &stack->head;
 	struct Node* newNode = malloc(sizeof *newNode);
 	NodeInit(newNode, data);
 
-	newNode->next = stack->head.next;
-	stack->head.next = newNode;
+	newNode->next = stack->head;
+	*headNode = newNode;
 }
 
 void* StackPop(struct Stack* stack, size_t size)
 {
-	if (stack->head.next == NULL)
+	if (stack->head == NULL)
 	{
 		void* newMemory = malloc(size);
 		memset(newMemory, 0, size);
 		return newMemory;
 	}
 
-	void* returnValue = stack->head.next->data;
-	struct Node* nextNode = stack->head.next->next;
-	free(stack->head.next);
-	stack->head.next = nextNode;
+	void* returnValue = stack->head->data;
+	struct Node* nextNode = stack->head->next;
+	struct Node** headNode = &stack->head;
+	free(stack->head);
+	*headNode = nextNode;
 
 	return returnValue;
 }
 
 void StackFree(struct Stack* stack, size_t size)
 {
-	while (stack->head.next != NULL)
+	while (stack->head != NULL)
 		StackPop(stack, size);
 }
